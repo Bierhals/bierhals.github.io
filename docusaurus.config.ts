061cf58909cdbbb6ce0ab6fa4cde1b92659205c5
Sven-Uwe Bierhals <sven-uwe@bierhals-online.de>
2025-10-29 20:27:11 +0000

    Erste Anpassungen

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Uwe\'s Entwickler-Notizen',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://bierhals.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -25,15 +25,15 @@ const config: Config = {
   organizationName: 'bierhals', // Usually your GitHub org/user name.
   projectName: 'bierhals.github.io', // Usually your repo name.
   trailingSlash: false,
-  
+
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'de',
+    locales: ['de'],
   },
 
   presets: [
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/bierhals/bierhals.github.io/blob/main',
         },
         blog: {
           showReadingTime: true,
@@ -56,7 +56,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/bierhals/bierhals.github.io/blob/main',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -76,34 +76,35 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Uwe\'s Notizen',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Uwe',
+        src: 'img/logo.png',
       },
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'artikelSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Artikel',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
+        {to: '/impressum', label: 'Impressum', position: 'left'},
+        /*{
+          href: 'https://github.com/bierhals',
           label: 'GitHub',
           position: 'right',
-        },
+        },*/
       ],
     },
     footer: {
       style: 'dark',
       links: [
-        {
+        /*{
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Artikel',
               to: '/docs/intro',
             },
           ],
@@ -137,9 +138,9 @@ const config: Config = {
               href: 'https://github.com/facebook/docusaurus',
             },
           ],
-        },
+        },*/
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Sven-Uwe Bierhals. Inhalte stehen und CC BY 4.0, sofern nicht anders angegeben.`,
     },
     prism: {
       theme: prismThemes.github,
---
+++ src/pages/impressum.md
@@ -0,0 +1,10 @@
+---
+title: Impressum
+---
+# Impressum
+
+**Sven-Uwe Bierhals**<br/>
+Salierstr. 90<br/>
+12524 Berlin
+
+sven-uwe[at]bierhals-online.de
